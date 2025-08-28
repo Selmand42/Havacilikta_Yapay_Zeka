b44de250e3e85b83f27dd4b2223694e23f175347
Selman Demir <Selmand42@users.noreply.huggingface.co>
2025-08-28 08:21:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 import gradio as gr
 from ultralytics import YOLO
+import torch
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
 
-# Modeli yükle
-model = YOLO("model.pt")
+model = YOLO("modelv12.pt")
 
 def predict(image):
-    results = model(image)
-    res_plotted = results[0].plot()  # Kutular çizilmiş numpy array
-    return res_plotted  # cv2.cvtColor ile dönüştürme yok
+    results = model(image, device=device)  # inference cihazını belirt
+    res_plotted = results[0].plot()
+    return res_plotted
 
 demo = gr.Interface(
     fn=predict,
     inputs=gr.Image(type="numpy"),
     outputs=gr.Image(type="numpy"),
     title="YOLO Detection",
-    description="Upload an image and get YOLO detection results"
+    description="Upload an image and get YOLO detection results",
+    examples=[
+        ["Frames/frames1.jpg"],
+        ["Frames/frames2.jpg"],
+        ["Frames/frames3.jpg"],
+        ["Frames/frames4.jpg"],
+        ["Frames/frames5.jpg"]
+    ]
 )
 
 demo.launch()
